c7c9535ca65035ed82a6c30f078e645a857ea4ba
teena1313 <tinalai.34@gmail.com>
2024-10-22 19:18:37 -0700

    added 2 new routes (blog and photos)

--- app/page.js
+++ app/page.js
@@ -1,19 +1,11 @@
+import Navbar from "./components/navbar";
+
 export default function Home() {
   return (
-    <nav className="border-b border-gray-800 sticky top-0 bg-gray-900 text-gray-100 z-10">
-      <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
-        <a href="/" className="font-medium text-lg md:hover:underline">
-          My Website
-        </a>
-        <ul className="flex items-center justify-end space-x-4 text-sm font-medium">
-          <li className="md:hover:underline">
-            <a href="/blog">Blog</a>
-          </li>
-          <li className="md:hover:underline">
-            <a href="/photos">Photos</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
+    <>
+      <h1 className="m-[10vw] text-violet-600 text-xl">Welcome!</h1>
+      <p>Thanks for visiting my website!</p>
+    </>
+
+  );
 }
---
+++ app/photos/page.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/navbar";
+import Image from "next/image";
+export default function Page() {
+    return (
+        <>
+            <Image></Image>
+            <p>photos :D</p>
+        </>
+    )
+}
