4c4d7f150fb036468eb34a275bfbe691ef69293f
teena1313 <tinalai.34@gmail.com>
2025-01-09 15:42:26 -0800

    fixed footer location

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-      <footer className="border-t text-center p-4 text-sm font-semibold bg-primary-100 text-primary-900 border-primary-200">
-        <p>Me &copy; {new Date().getFullYear()}</p>
+      <footer className="fixed bottom-0 w-screen border-t text-center p-4 text-sm font-semibold bg-primary-100 text-primary-900 border-primary-200">
+        <p>Tina Lai &copy; {new Date().getFullYear()}</p>
       </footer>
     );
   }
